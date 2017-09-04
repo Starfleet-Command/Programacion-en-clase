d59f58ebd0106ebd72f1541367e010676388842e
Starfleet-Command <juanfgort@gmail.com>
2017-09-04 11:57:35 -0500

    Clase semana 4

---
+++ Condiciones.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Condiciones
+{
+	public static void main(String[] args)
+	{
+		int temperatura;
+		String salida;
+		Scanner lectura = new Scanner(System.in);
+
+		System.out.println("Escribe la temperatura del auto en numeros enteros");
+
+		temperatura = lectura.nextInt();
+
+		if (temperatura>=110  || temperatura< -10)
+		{
+		System.out.println("Alerta: Explosion inminente; Uso continuo del sistema no recomendado");
+		System.out.println("Revise el nivel de aceite y su temperatura");
+	}
+	else
+	{
+		System.out.println("Todo dentro de parametros aceptables. Continue");
+	}
+lectura.nextLine();
+	System.out.println("Escribe s para salir");
+	salida = lectura.nextLine();
+	if(salida.equals("S")|| salida.equals("s")) // Puedes aplicar equals.IgnoreCase para no hacer esto doble
+	{
+	System.out.println("Hasta la vista,baby");
+	}
+
+	}
+}
+
+
+
+
--- Metodo2.java
+++ Metodo2.java
@@ -5,6 +5,7 @@ public class Metodo2
   public static void main(String[] args)
 	{
 		 System.out.printf("El cuadrado de %f es %f", 4.0, cuadrado(4.0));
+		 System.out.printf("%.2f %n", numero1);
 
 		}
 		public static double cuadrado(double numero)
@@ -14,4 +15,8 @@ public class Metodo2
 			return cuadrado;
 }
 
+	public static void imprimeHola()
+	{
+		System.out.printf("Hola a todos");
+	}
 }
